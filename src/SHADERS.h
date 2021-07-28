d166d40e516791640ad7264109f27be6c658a921
Mike Santiago <miketheripper1@gmail.com>
2021-07-28 17:33:22 -0400

    Tweak stock shaders to support scale and rot matrices

--- src/SHADERS.h
+++ src/SHADERS.h
@@ -17,22 +17,41 @@ static const char vFragmentString[] =
 
 #else
 
+
+/*
+Note to future self:
+
+GLSL requires that any variables be used at least once. 
+ If not, you won't be able to get their attrib ID.
+*/
+
 static const char vShaderString[] = 
-     "attribute vec4 vPosition;    \n"
+     "attribute vec2 vPosition;    \n"
+     "attribute vec2 vTexCoord;    \n"
      "attribute vec4 vColor;      \n"
      "varying vec4 fragColor;           \n"
-     "uniform mat4 mvp;"
+     "varying vec2 texCoord;            \n"
+     "uniform mat4 mvp;\n"
+     "uniform mat4 _rot;\n"
+     "uniform mat4 _scale;\n"
       "void main()                  \n"
       "{                            \n"
-      "   gl_Position = mvp * vPosition;  \n"
+      "   gl_Position = mvp * _rot * _scale * vec4(vPosition.xy, 0, 1);  \n"
       "   fragColor = vColor;          \n"
+      "   texCoord = vTexCoord;         \n"
       "}                            \n";
 
 static const char vFragmentString[] =   
-      "varying vec4 fragColor;"
+      "varying vec4 fragColor;\n"
+      "varying vec2 texCoord;\n"
+      "uniform sampler2D ourTexture;"
+      "uniform bool useTexture;"
       "void main()                                  \n"
       "{                                            \n"
-      "  gl_FragColor = fragColor;\n"
+      " if(useTexture)\n"
+      "     gl_FragColor = texture2D(ourTexture, texCoord) * fragColor;\n"
+      " else\n"
+      "     gl_FragColor = fragColor;\n"
       "}           ";
 
 #endif
